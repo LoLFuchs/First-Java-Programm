6f46e5838b31e3f833115cd31b11a1d8031319d2
Dominik Engelen <dominik.engelen@online.de>
2023-10-13 12:01:47 +0200

    Added sum.java

---
+++ sum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int math(int a, int b, String op) {
+        Integer End = 0;
+        switch (op) {
+            case "+":
+                End = a + b;
+                break;
+            case "-":
+                End = a - b;
+                break;
+            case "/":
+                End = a / b;
+                break;
+            case "*":
+                End = a * b;
+                break;
+            default:
+                System.out.println("Wrong Operation");
+                break;
+        }
+        return End;
+    }
+
+    public static void main(String[] args) {
+        Scanner newObj = new Scanner(System.in);
+
+        System.out.println("enter number 1");
+        Integer ZahlEins = newObj.nextInt();
+
+        System.out.println("enter number 2");
+        Integer ZahlZwei = newObj.nextInt();
+
+        System.out.println("enter operation (+ - / *)");
+        String Operation = newObj.next();
+
+        Main obj = new Main();
+        Integer result = obj.math(ZahlEins, ZahlZwei, Operation);
+        System.out.println("Result: " + result);
+    }
+}
